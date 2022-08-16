1fa0d3091e795b8c690a75feedabca314121eae5
Simion <simion.okola@student.moringaschool.com>
2022-08-17 02:19:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(event = 'roller-skate'){
+    return `This Saturday, I want to ${event}!`;
+}
+saturdayFun('bathe my dog');
+
+function mondayWork(work = 'go to the office'){
+    return `This Monday, I will ${work}.`
+}
+mondayWork('relax with my friends');
+
+function wrapAdjective(star = "*"){
+    return function (coder = "special"){
+     return `You are ${star}${coder}${star}!`;
+    }
+}
+wrapAdjective()('a hardworker');
+
